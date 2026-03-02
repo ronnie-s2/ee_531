3e4f7f0076da2ceb5d13e40443168037860df92a
Ronnie <ronnie.sidhu2004@gmail.com>
2026-03-01 17:15:23 -0800

    Created testbench files, yet to be completed.

---
+++ ee_531.srcs/sim_1/new/cache_hierarchy_tb.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 03/01/2026 05:13:06 PM
+// Design Name: 
+// Module Name: cache_hierarchy_tb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module cache_hierarchy_tb(
+
+    );
+endmodule
---
+++ ee_531.srcs/sim_1/new/l3_dram_tb.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 03/01/2026 05:13:06 PM
+// Design Name: 
+// Module Name: l3_dram_tb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module l3_dram_tb(
+
+    );
+endmodule
