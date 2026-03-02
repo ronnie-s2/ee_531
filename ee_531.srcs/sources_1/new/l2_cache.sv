3ff4066d7639b8ee523ac5aa43c0e9ddba9b4174
Ronnie <ronnie.sidhu2004@gmail.com>
2026-03-01 16:49:23 -0800

    Added modular address slicing to L1-L3 caches.

--- ee_531.srcs/sources_1/new/l2_cache.sv
+++ ee_531.srcs/sources_1/new/l2_cache.sv
@@ -51,10 +51,30 @@ module l2_cache(
 
   // Controller FSM state
   cache_state_e state;
+  
+  // Address slicing
+  logic [TAG_BITS-1:0]    addr_tag;
+  logic [INDEX_BITS-1:0]  addr_index;
+  logic [OFFSET_BITS-1:0] addr_offset;
+  logic [$clog2(LINE_BYTES/(DATA_WIDTH/8))-1:0] word_index; // 4 bits for 16 words
+
+  always_comb begin
+    // tag = top TAG_BITS bits
+    addr_tag    = cpu_req.addr[ADDR_WIDTH-1 -: TAG_BITS];
+
+    // index = next INDEX_BITS above offset
+    addr_index  = cpu_req.addr[OFFSET_BITS + INDEX_BITS-1 -: INDEX_BITS];
+
+    // offset = lowest OFFSET_BITS bits
+    addr_offset = cpu_req.addr[OFFSET_BITS-1:0];
+
+    // word index = select which word in the line
+    word_index  = addr_offset[OFFSET_BITS-1 : OFFSET_BITS-$clog2(LINE_BYTES/(DATA_WIDTH/8))];
+  end
 
   // -------------------------
   // TODO:
-  // - Address slicing: tag/index/offset
+  // - Address slicing: tag/index/offset (DONE)
   // - Hit/miss logic
   // - Pseudo-LRU replacement logic
   // - FSM transitions: IDLE, LOOKUP, MISS, REFILL, WRITEBACK
